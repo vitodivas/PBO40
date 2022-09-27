7f01311097229a6ca492c1fa078924477772342c
Ilyasha Vito Divas <114458834+vitodivas@users.noreply.github.com>
2022-09-27 11:08:25 +0700

    Add files via upload

---
+++ IfThen.java
@@ -0,0 +1,10 @@
+package com.smkn40jakarta.percabangan;
+
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        }
+    }
+}
