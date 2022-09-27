3645c39bdf83b24396bd6e93ab6063bb44cecb83
Ilyasha Vito Divas <114458834+vitodivas@users.noreply.github.com>
2022-09-27 11:14:53 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-package com.smkn40jakarta.perulangan;
+package smkn40jakarray.com;
 
 /**
  * Hello world!
@@ -8,6 +8,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+    {
+        int[] kelas = {10, 11, 12, };
+    
+    System.out.println("Urutan kelas :");
+    System.out.println(kelas[0]);
+    System.out.println(kelas[1]);
+    System.out.println(kelas[2]);
+  
+
+    String[] jurusan = {"RPL", "MM1", "MM2" , "BDP", "AKL", "OTKP" };
+    System.out.println(" ");
+
+     
+    System.out.println("JURUSAN DI SMKN 40 :");
+    System.out.println(jurusan[0]); 
+    System.out.println(jurusan[1]); 
+    System.out.println(jurusan[2]); 
+    System.out.println(jurusan[3]); 
+    System.out.println(jurusan[4]);
+    System.out.println(jurusan[5]);
+    }
     }
 }
