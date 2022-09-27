3361e5a59abf4d139407a3294ec7ad19afdb12be
Ilyasha Vito Divas <114458834+vitodivas@users.noreply.github.com>
2022-09-27 11:09:54 +0700

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,10 @@
+public class Print01{
+    public static void main(String[]args) {
+        System.out.println("Dicetak pakai println()");
+        System.out.println("Ini juga dicetak pakai println()");
+        System.out.print("Ini dicetak dengan print()");
+        System.out.print(" dan ini juga dicetak dengan print()");
+        System.out.printf("Ini dicetak dengan printf()");
+        System.out.printf(" dan ini juga dicetak dengan printf()");
+    }
+}
