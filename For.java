7b85811f484c35192f77a8f098623c2af4f4b7f6
Ilyasha Vito Divas <114458834+vitodivas@users.noreply.github.com>
2022-09-27 11:07:56 +0700

    Add files via upload

---
+++ For.java
@@ -0,0 +1,9 @@
+package com.smkn40jakarta.perulangan;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka : " + i);
+        }
+    }
+}
