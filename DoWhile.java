da8aaf9e66c08baf665871daeee0d09b3df05325
Ilyasha Vito Divas <114458834+vitodivas@users.noreply.github.com>
2022-09-27 10:47:28 +0700

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,11 @@
+package com.smkn40jakarta.perulangan;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int value = 1;
+        do {
+            System.out.println("Angka : " + value);
+            value++;
+        } while (value <= 10);
+    }
+}
